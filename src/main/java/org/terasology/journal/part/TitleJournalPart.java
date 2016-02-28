4904b57a5ba041380603b7e0beb12b5109678123
Rasmus Praestholm <cervator@gmail.com>
2016-02-27 20:28:54 -0500

    Move Assets class from org.terasology.asset to org.terasology.utilities.

--- src/main/java/org/terasology/journal/part/TitleJournalPart.java
+++ src/main/java/org/terasology/journal/part/TitleJournalPart.java
@@ -15,7 +15,7 @@
  */
 package org.terasology.journal.part;
 
-import org.terasology.asset.Assets;
+import org.terasology.utilities.Assets;
 import org.terasology.journal.JournalManager;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector2i;
