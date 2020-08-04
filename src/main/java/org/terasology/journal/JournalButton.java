90b6ca3b942dd6303bbb6e7486f49eab25b5ad97
Michael Pollind <polli104@mail.chapman.edu>
2020-08-04 11:34:54 -0700

    fix: Use Terasology key reference for JournalButton (#10)

--- src/main/java/org/terasology/journal/JournalButton.java
+++ src/main/java/org/terasology/journal/JournalButton.java
@@ -15,16 +15,15 @@
  */
 package org.terasology.journal;
 
-import org.lwjgl.input.Keyboard;
 import org.terasology.input.BindButtonEvent;
 import org.terasology.input.DefaultBinding;
 import org.terasology.input.InputType;
+import org.terasology.input.Keyboard;
 import org.terasology.input.RegisterBindButton;
 
 /**
- * @author Marcin Sciesinski <marcins78@gmail.com>
  */
 @RegisterBindButton(id = "openJournal", description = "Open journal", category = "interaction")
-@DefaultBinding(type = InputType.KEY, id = Keyboard.KEY_J)
+@DefaultBinding(type = InputType.KEY, id = Keyboard.KeyId.J)
 public class JournalButton extends BindButtonEvent {
 }
